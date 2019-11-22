33b2d361296761f734e7878b02a2b0212948c2fd
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-11-22 08:01:59 -0600

    Updates to first two stages of dataset ingest logic.

--- tasks/normalize-raw-dataset/entrypoint.sh
+++ tasks/normalize-raw-dataset/entrypoint.sh
@@ -3,18 +3,32 @@
 INPUT_DIR=/mnt/inputs
 OUTPUT_DIR=/mnt/outputs
 
+echo "Starting dataset normalization..."
+
 mkdir -p "${OUTPUT_DIR}"
 
-# Process python
-echo "Normalizing python files..."
-find "${INPUT_DIR}" -type f -name "*.py" \
-  | parallel --bar --will-cite python3 /src/function-parser/function_parser/parser_cli.py \
-      --language python {} 
-echo "  + Complete!"
+echo "  - Normalizing 'test.jsonl.gz'..."
+cat /mnt/inputs/test.jsonl.gz \
+  | gzip -cd \
+  | python3 /src/function-parser/function_parser/parser_cli.py \
+  | gzip \
+> /mnt/outputs/test.jsonl.gz
+echo "    + Normalized!"
 
-# Process java
-echo "Normalizing java files..."
-find "${INPUT_DIR}" -type f -name "*.java" \
-  | parallel --bar --will-cite python3 /src/function-parser/function_parser/parser_cli.py \
-      --language java {}
-echo "  + Complete!"
+echo "  - Normalizing 'train.jsonl.gz'..."
+cat /mnt/inputs/train.jsonl.gz \
+  | gzip -cd \
+  | python3 /src/function-parser/function_parser/parser_cli.py \
+  | gzip \
+> /mnt/outputs/train.jsonl.gz
+echo "    + Normalized!"
+
+echo "  - Normalizing 'valid.jsonl.gz'..."
+cat /mnt/inputs/valid.jsonl.gz \
+  | gzip -cd \
+  | python3 /src/function-parser/function_parser/parser_cli.py \
+  | gzip \
+> /mnt/outputs/valid.jsonl.gz
+echo "    + Normalized!"
+
+echo "  + Dataset normalization complete!"
