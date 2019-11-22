de70c87f2d55dc35dec33d2f4c73a4879b89327b
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-11-22 07:59:12 -0600

    More utility scripts.

---
+++ scripts/danger-clear-normalized-datasets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run -it --rm \
+  -v "${DIR}/../datasets/normalized:/mnt" \
+  --entrypoint bash \
+  debian:9 \
+    -c 'rm -rf /mnt/c2s/* /mnt/csn/*'
