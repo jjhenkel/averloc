834c26fc4ffc995d80a1a5e0874a2a8b7a0778f8
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-11-24 07:57:06 -0600

    Working on stage-1 preprocess. All good except for java-med.

--- tasks/preprocess-dataset-c2s-1/entrypoint.sh
+++ tasks/preprocess-dataset-c2s-1/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# Need to modify Java to read in X.jsonl.gz files
+mkdir -p /mnt/outputs/test
+mkdir -p /mnt/outputs/train
+mkdir -p /mnt/outputs/valid
+
+mkdir -p /tmp/java
+mkdir -p /tmp/python
+
 /astminer/gradlew processMyExample --args="/mnt/inputs/test.jsonl.gz"
+mv -T "/tmp/${1}" /mnt/outputs/test
+
 /astminer/gradlew processMyExample --args="/mnt/inputs/train.jsonl.gz"
+mv -T "/tmp/${1}" /mnt/outputs/train
+
 /astminer/gradlew processMyExample --args="/mnt/inputs/valid.jsonl.gz"
+mv -T "/tmp/${1}" /mnt/outputs/valid
