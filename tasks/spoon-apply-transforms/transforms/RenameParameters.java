a4133e1fc052a2670cc7d1e21d90225e48f8ef9a
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-07 21:48:24 -0600

    Big changes: refactored transformers. Now have renaming/shuffling of local vars and parameters and nested call params.

---
+++ tasks/spoon-apply-transforms/transforms/RenameParameters.java
@@ -0,0 +1,28 @@
+package transforms;
+
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.*;
+import spoon.reflect.reference.*;
+
+public class RenameParameters extends Renamer<CtParameter> {
+    // Parameters to this renaming transform
+    final static int NAME_MIN_LENGTH = 1;
+    final static int NAME_MAX_LENGTH = 5;
+    final static double RENAME_PERCENT = 1.0;
+    final static Boolean SHUFFLE_MODE = false;
+
+	@Override
+	public void transform(CtExecutable method) {
+        // Get setup for renaming
+        setDefs(getChildrenOfType(method, CtParameter.class));
+        setSubtokens(AverlocTransformer.TOP_N_TARGET_SUBTOKENS);
+        
+        // Select some percentage of things to rename
+        takePercentage(RENAME_PERCENT);
+
+        // Build new names and apply them
+        applyRenaming(method, generateRenaming(
+            SHUFFLE_MODE, NAME_MIN_LENGTH, NAME_MAX_LENGTH
+        ));
+	}
+}
