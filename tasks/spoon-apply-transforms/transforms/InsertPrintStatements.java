9d177a1a8ddc7829bb89402609de874ddd35e2c1
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-08 11:59:32 -0600

    Added insert prints transform. Had idea for other transform.

---
+++ tasks/spoon-apply-transforms/transforms/InsertPrintStatements.java
@@ -0,0 +1,47 @@
+package transforms;
+
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.*;
+import spoon.reflect.reference.*;
+
+import java.util.*;
+import java.lang.Math;
+
+public class InsertPrintStatements extends AverlocTransformer {
+  // Parameters to this renaming transform
+  final static int MIN_INSERTIONS = 1;
+  final static int MAX_INSERTIONS = 3;
+  final static int LITERAL_MIN_LENGTH = 1;
+  final static int LITERAL_MAX_LENGTH = 5;
+
+	@Override
+	public void transform(CtExecutable method) {
+    Random rand = new Random();
+
+    int numInsertions = rand.nextInt(
+      (MAX_INSERTIONS - MIN_INSERTIONS) + 1
+    ) + MIN_INSERTIONS;
+
+    for (int i = 0; i < numInsertions; i++) {
+      int literalLength = rand.nextInt(
+        (LITERAL_MAX_LENGTH - LITERAL_MIN_LENGTH) + 1
+      ) + LITERAL_MIN_LENGTH;
+
+      Collections.shuffle(AverlocTransformer.TOP_N_TARGET_SUBTOKENS);
+
+      String literal = camelCased(
+        AverlocTransformer.TOP_N_TARGET_SUBTOKENS.subList(0, literalLength)
+      );
+
+      CtCodeSnippetStatement snippet = getFactory().Core().createCodeSnippetStatement();
+      snippet.setValue(String.format("System.out.println(\"%s\")", literal));
+
+      // Insert randomly begin/end
+      if (rand.nextBoolean()) {
+        method.getBody().insertBegin(snippet);
+      } else {
+        method.getBody().insertEnd(snippet);
+      }
+    }
+	}
+}
