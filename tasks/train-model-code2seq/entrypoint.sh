513f1a13e85d96bb033a4fc2ee42fc1deeb15327
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-14 21:44:12 -0600

    More merging.

--- tasks/train-model-code2seq/entrypoint.sh
+++ tasks/train-model-code2seq/entrypoint.sh
@@ -4,7 +4,6 @@ mkdir -p /models
 
 set -ex
 
-<<<<<<< HEAD
 # python3 -u /code2seq/code2seq.py \
 #   --data "/datasets/preprocessed/ast-paths/${DATASET_NAME}/data" \
 #   --test "/datasets/preprocessed/ast-paths/${DATASET_NAME}/data.val.c2s" \
@@ -13,9 +12,3 @@ set -ex
 python3 /code2seq/code2seq.py \
   --load /app/models/java-large-model/model_iter52.release \
   --test /mnt/inputs/data.test.c2s
-=======
-python3 -u /code2seq/code2seq.py \
-  --data "/datasets/preprocessed/ast-paths/${DATASET_NAME}/data" \
-  --test "/datasets/preprocessed/ast-paths/${DATASET_NAME}/data.val.c2s" \
-  --save_prefix /models
->>>>>>> 456449a5524071af4774e6f0817a0650f3d7bc08
