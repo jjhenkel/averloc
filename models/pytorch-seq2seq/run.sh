2e9e3994ca7fd0e0cce0ec0bbed867b114959843
goutham7r <goutham7r@gmail.com>
2020-01-18 04:31:55 +0000

    small changes

--- models/pytorch-seq2seq/run.sh
+++ models/pytorch-seq2seq/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -x
 # python sample.py --train_path data/en-fr/europarl-v7.fr-en.val.tsv --dev_path data/en-fr/europarl-v7.fr-en.val.tsv
-python sample.py --train_path data/en-fr/europarl-v7.fr-en.train.tsv --dev_path data/en-fr/europarl-v7.fr-en.val.tsv
+python train.py --train_path data/java-small/transforms.Identity/train.tsv --dev_path data/java-small/transforms.Identity/valid.tsv --expt_name java_small_identity_lstm
+# python train.py --train_path data/java-small/transforms.Identity/train.tsv --dev_path data/java-small/transforms.Identity/valid.tsv --expt_name java_small_identity  --resume --expt_dir experiment/java_small_identity --load_checkpoint Best_F1
