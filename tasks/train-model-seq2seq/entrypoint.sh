cf8e2a391c1ef34ea5a0e1d0943bb5b9ecd819b4
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-23 14:30:50 -0600

    And now a typo fix.

--- tasks/train-model-seq2seq/entrypoint.sh
+++ tasks/train-model-seq2seq/entrypoint.sh
@@ -8,6 +8,6 @@ python /model/train_adv.py \
   --train_path /mnt/inputs/train.tsv \
   --dev_path /mnt/inputs/valid.tsv \
   --expt_name lstm \
-  --exprt_dir /mnt/outputs $@
+  --expt_dir /mnt/outputs $@
 
 # python train.py --train_path data/java-small/transforms.Identity/train.tsv --dev_path data/java-small/transforms.Identity/valid.tsv --expt_name java_small_identity  --resume --expt_dir experiment/java_small_identity --load_checkpoint Best_F1
