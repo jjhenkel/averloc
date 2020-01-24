f7b7df0fe796691901978c1a13ef25708f828813
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-24 09:26:29 -0600

    Typo fix

--- tasks/test-model-seq2seq/entrypoint.sh
+++ tasks/test-model-seq2seq/entrypoint.sh
@@ -2,17 +2,21 @@
 
 set -ex
 
+cat /mnt/inputs/test.tsv | awk -F'\t' '{ print $2 "\t" $3 }' > /test-fixed.tsv
+
 python /model/evaluate.py \
-  --data_path /mnt/inputs/test.tsv \
+  --data_path /test-fixed.tsv \
   --expt_dir /models/lstm \
   --output_dir /mnt/outputs \
   --load_checkpoint Best_F1 \
   --output_fname results-test \
     $@
 
-if [ -f /mnt/inputs/data.baseline.c2s ]; then
+if [ -f /mnt/inputs/baseline.tsv ]; then
+  cat /mnt/inputs/baseline.tsv | awk -F'\t' '{ print $2 "\t" $3 }' > /baseline-fixed.tsv
+  
   python /model/evaluate.py \
-    --data_path /mnt/inputs/baseline.tsv \
+    --data_path /baseline-fixed.tsv \
     --expt_dir /models/lstm \
     --output_dir /mnt/outputs \
     --load_checkpoint Best_F1 \
