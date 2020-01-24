cb217bbaef3e480b2dffd2698083f04152d300f5
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-01-24 11:03:55 -0600

    New results script.

---
+++ scripts/get-results-seq2seq-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+OUTPUT_DIR="results/01-24-2020"
+
+SETS="c2s/java-small csn/java csn/python"
+
+MODELS="adversarial adversarial-all normal"
+
+TESTS_SOURCE="datasets/transformed/preprocessed/tokens"
+
+trap "echo 'CTRL-C Pressed. Quiting...'; exit;" SIGINT SIGTERM
+
+for the_set in ${SETS}; do
+  for the_model in ${MODELS}; do
+    for the_test in $(find ${TESTS_SOURCE}/${the_set} -mindepth 1 -maxdepth 1 -type d); do
+      results_dir="${OUTPUT_DIR}/${the_set}/${the_model}/$(basename ${the_test})"
+    
+      if [ "$(basename ${the_test})" != "transforms.Identity" ]; then 
+
+        f1=$(cat "${results_dir}/results-test_stats.txt" | grep -Po 'f1: \d+\.\d+' | awk '{ print $2}')
+        f1_b=$(cat "${results_dir}/results-baseline_stats.txt" | grep -Po 'f1: \d+\.\d+' | awk '{ print $2}')
+
+        echo "${the_set},${the_model},$(basename ${the_test}),$(awk "BEGIN{print ${f1}-${f1_b}}")"
+
+      fi
+
+    done
+  done
+done
