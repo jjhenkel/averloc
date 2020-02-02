8dce82de195b1b33907f67f1e69ff1f59dc36057
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-02-01 20:57:11 -0600

    Allow trained-models prefixing.

--- scripts/normal-eval.sh
+++ scripts/normal-eval.sh
@@ -6,19 +6,19 @@ THE_DS=$1
 
 DATASET_NAME=datasets/transformed/preprocessed/tokens/${THE_DS}/transforms.Identity \
 RESULTS_OUT=test-results/${THE_DS}/normal/normal \
-MODELS_IN=trained-models/seq2seq/${THE_DS}/normal \
+MODELS_IN=${2}trained-models/seq2seq/${THE_DS}/normal \
 ARGS="--no-attack --save ${ARGS}" \
 time make test-model-seq2seq
 
 DATASET_NAME=datasets/transformed/preprocessed/tokens/${THE_DS}/transforms.Identity \
 RESULTS_OUT=test-results/${THE_DS}/adversarial-all/normal \
-MODELS_IN=trained-models/seq2seq/${THE_DS}/adversarial-all \
+MODELS_IN=${2}trained-models/seq2seq/${THE_DS}/adversarial-all \
 ARGS="--no-attack --save ${ARGS}" \
 time make test-model-seq2seq
 
 DATASET_NAME=datasets/transformed/preprocessed/tokens/${THE_DS}/transforms.Identity \
 RESULTS_OUT=test-results/${THE_DS}/adversarial-one-step/normal \
-MODELS_IN=trained-models/seq2seq/${THE_DS}/adversarial-one-step \
+MODELS_IN=${2}trained-models/seq2seq/${THE_DS}/adversarial-one-step \
 ARGS="--no-attack --save ${ARGS}" \
 time make test-model-seq2seq
 
